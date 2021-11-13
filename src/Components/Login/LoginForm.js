09dee82829014a89219952a8499875bb5ec5d29c
Felipe Escarassatti <felipeescarassatti@gmail.com>
2021-11-13 12:07:16 -0300

    form component

---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassaword] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ username, password });
+    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        event.preventDefault();
+        console.log({ username, password });
+        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        })
+          .then((response) => {
+            console.log(response);
+            return response.json();
+          })
+          .then((json) => {
+            console.log(json);
+            return json;
+          });
+
+        return json;
+      });
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </div>
+  );
+};
+
+export default LoginForm;
