67401e9a3c6b7247510e2a26f139ceb987ef26bc
Felipe Escarassatti <felipeescarassatti@gmail.com>
2021-11-23 07:38:16 -0300

    iniciando feed modal

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div className={styles.Footer}>Footer</div>;
+  return <div className={`${styles.Footer} container`}>Footer</div>;
 };
 
 export default Footer;
